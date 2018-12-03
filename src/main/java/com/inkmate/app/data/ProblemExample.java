58437200b0d2c68b35674a2e913f4c7f72a255c2
sanautiyal <sanautiyal@homeaway.com>
2018-12-03 22:05:17 +0530

    Tags now sent as array

--- src/main/java/com/inkmate/app/data/ProblemExample.java
+++ src/main/java/com/inkmate/app/data/ProblemExample.java
@@ -5,34 +5,22 @@ import lombok.Data;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity
-@Table(name="ProblemExample")
 @Data
 public class ProblemExample {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="Id")
     private long id;
 
-    @ManyToOne
     private Problem problem;
 
-    @Column(name="ProblemId")
     private long problemId;
 
-    @Column(name="Description")
     private String description;
 
-    @Column(name="Data")
     private String data;
 
-    @Column(name="Visualization")
     private String visualization;
 
-    @Column(name="Result")
     private String result;
 
-    @Column(name="ResultExplaination")
     private String resultExplaination;
 }
--- src/main/java/com/inkmate/app/data/Solution.java
+++ src/main/java/com/inkmate/app/data/Solution.java
@@ -6,44 +6,27 @@ import javax.persistence.*;
 
 import java.util.Date;
 
-@Entity
-@Table(name="Solution")
 @Data
 public class Solution {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="SolutionId")
     private long solutionId;
 
-    @Column(name="ProblemId")
     private long problemId;
 
-    @Column(name="Description")
     private String description;
 
-    @Column(name="Class")
     private String className;
 
-    @Column(name="Language")
     private String langauge;
 
-    @Column(name="Email")
     private String email;
 
-    @Column(name="Author")
     private String author;
 
-    @Column(name="RecordCreateTime")
-    @Temporal(TemporalType.TIMESTAMP)
     private Date recordCreateTime;
 
-    @Column(name="RecordUpdateTime")
-    @Temporal(TemporalType.TIMESTAMP)
     private Date recordUpdateTime;
 
-    @OneToOne()
-    @JoinColumn(name="SolutionId")
     private SolutionReaction solutionReaction;
 
     private String solutionContent;
