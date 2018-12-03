9cb8bfc5e8386990ccb0c5982a6f8fe46a497625
sanautiyal <sanautiyal@homeaway.com>
2018-12-03 22:01:01 +0530

    Tags now sent as array

--- src/main/java/com/inkmate/app/data/Problem.java
+++ src/main/java/com/inkmate/app/data/Problem.java
@@ -8,45 +8,27 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Entity
-@Table(name="Problem")
-@Data
 public class Problem {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="ProblemId")
     private long problemId;
 
-    @Column(name="Title")
     private String title;
 
-    @Column(name="ProblemDescription")
     private String problemDescription;
 
-    @Column(name="Tags")
     private List<String> tags;
 
-    @Column(name="Category")
     private String category;
 
-    @Column(name="Email")
     private String email;
 
-    @Column(name="Author")
     private String author;
 
-    @Column(name="DifficultyLevel")
     private String difficultyLevel;
 
-    @Column(name="RecordCreateTime")
-    @Temporal(TemporalType.TIMESTAMP)
     private Date recordCreateTime;
 
-    @Column(name="RecordUpdateTime")
-    @Temporal(TemporalType.TIMESTAMP)
     private Date recordUpdateTime;
 
-    @OneToMany(mappedBy="problem")
     private Set<ProblemExample> examples = new HashSet<ProblemExample>();
 }
